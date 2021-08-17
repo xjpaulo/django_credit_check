bd541f45a7456b0e4a7bd61366bc37e3685dd3b2
João Paulo Cardoso <jpaulo.jpc@gmail.com>
2021-08-17 19:41:47 -0300

    changing responses

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import CreditCheck, Results
 
 urlpatterns = [
     path('credit_check/', CreditCheck.as_view(), name='credit check'),
-    path('results/<str:token>/', Results.as_view(), name='results'),
+    path('results/<str:ticket>/', Results.as_view(), name='results'),
 ]
