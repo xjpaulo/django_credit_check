a90cfec01f9c642874f942ab820393efebf5756e
João Paulo Cardoso <jpaulo.jpc@gmail.com>
2021-08-17 20:34:32 -0300

    changing responses

--- api/tasks.py
+++ api/tasks.py
@@ -8,11 +8,15 @@ logger = get_task_logger(__name__)
 @shared_task
 def validate_credit(user_age, credit_value):
     time.sleep(60)
+    message_return = message_log = ''
     if user_age >= 18 and credit_value < 100000:
-        message = f'Credit approved: Age {user_age} is >= 18 and credit value {credit_value} is <= 100000.00'
+        message_log = f'Credit approved: Age {user_age} is >= 18 and credit value {credit_value} is <= 100000.00'
+        message_return = 'Credit approved: All values match the requirements.'
     elif user_age < 18:
-        message = f'Credit not approved: Age {user_age} is < 18'
+        message_log = f'Credit not approved: Age {user_age} is < 18'
+        message_return = 'Credit not approved: Minimum age allowed is 18.'
     elif credit_value > 100000:
-        message = f'Credit not approved: Credit value {credit_value} is > 100000'
-    logger.info(message)
-    return message
+        message_log = f'Credit not approved: Credit value {credit_value} is > 100000'
+        message_return = 'Credit not approved: Maximum credit value allowed is 100000.'
+    logger.info(message_log)
+    return message_return
