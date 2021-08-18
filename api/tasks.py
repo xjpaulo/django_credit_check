10cff68b68b29539e7919c59bd5b9a8f5ef0e0c6
João Paulo Cardoso <jpaulo.jpc@gmail.com>
2021-08-18 10:10:05 -0300

    task exceptions

--- api/tasks.py
+++ api/tasks.py
@@ -9,9 +9,9 @@ logger_celery = get_task_logger(__name__)
 
 @shared_task(soft_time_limit=50, time_limit=60)
 def validate_credit(user_age, credit_value):
-    logger_celery.debug('Validation task started.')
+    logger_celery.info('Validation task started.')
     try:
-        time.sleep(60)
+        time.sleep(40)
         message_return = detailed_log = ''
         if user_age >= 18 and credit_value < 100000:
             detailed_log = f'Credit approved: Age {user_age} is >= 18 and credit value {credit_value} is <= 100000.00'
@@ -22,8 +22,8 @@ def validate_credit(user_age, credit_value):
         elif credit_value > 100000:
             detailed_log = f'Credit not approved: Credit value {credit_value} is > 100000'
             message_return = 'Credit rejected: Maximum credit value allowed is 100000.'
-        logger_celery.debug(f'Validation task finished: {detailed_log}')
-    except SoftTimeLimitExceeded as e:
+        logger_celery.info(f'Validation task finished: {detailed_log}')
+    except SoftTimeLimitExceeded:
         message_return = 'An error occurred when running the validation task.'
-        logger_celery.critical(f'Validation task aborted: {e}')
+        logger_celery.warning(f'Validation task aborted due to SoftTimeLimitExceeded.')
     return message_return
