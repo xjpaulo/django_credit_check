04a2e0be5df45cf4d93f85c456f1eac03fcef741
João Paulo Cardoso <jpaulo.jpc@gmail.com>
2021-08-18 15:52:12 -0300

    creating some tests

--- api/tasks.py
+++ api/tasks.py
@@ -9,20 +9,20 @@ logger_celery = get_task_logger(__name__)
 
 @shared_task(soft_time_limit=50, time_limit=60)
 def validate_credit(user_age, credit_value):
-    logger_celery.info('Validation task started.')
+    logger_celery.debug('Validation task started.')
     try:
         time.sleep(40)
         message_return = detailed_log = ''
         if user_age >= 18 and credit_value < 100000:
-            detailed_log = f'Credit approved: Age {user_age} is >= 18 and credit value {credit_value} is <= 100000.00'
+            detailed_log = f'Credit approved: Age {user_age} is >= 18 and credit value {credit_value} is < 100000.00'
             message_return = 'Credit approved: All values match the requirements.'
         elif user_age < 18:
             detailed_log = f'Credit not approved: Age {user_age} is < 18'
             message_return = 'Credit rejected: Minimum age allowed is 18.'
-        elif credit_value > 100000:
-            detailed_log = f'Credit not approved: Credit value {credit_value} is > 100000'
-            message_return = 'Credit rejected: Maximum credit value allowed is 100000.'
-        logger_celery.info(f'Validation task finished: {detailed_log}')
+        elif credit_value >= 100000:
+            detailed_log = f'Credit not approved: Credit value {credit_value} is >= 100000'
+            message_return = 'Credit rejected: Maximum credit value allowed is 99999.'
+        logger_celery.debug(f'Validation task finished: {detailed_log}')
     except SoftTimeLimitExceeded:
         message_return = 'An error occurred when running the validation task.'
         logger_celery.warning(f'Validation task aborted due to SoftTimeLimitExceeded.')
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import json
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CreditTestCase(APITestCase):
+    def setUp(self):
+        self.credit_check_url = '/api/v1/credit/'
+
+    def test_credit_check_valid_payload(self):
+        valid_credit_data = {
+            'user_age': 19,
+            'credit_value': 50000
+        }
+        response = self.client.post(self.credit_check_url, data=valid_credit_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
