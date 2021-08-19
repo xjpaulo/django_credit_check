3dd757dae2610202f7b2af3e97da4a5ef605aae2
JPaulo <jpaulo.jpc@gmail.com>
2021-08-19 00:03:12 -0300

    modified tests

--- api/tests.py
+++ api/tests.py
@@ -1,19 +1,49 @@
 import json
 from rest_framework import status
 from rest_framework.test import APISimpleTestCase
+import logging
+from .serializers import CreditCheckSerializer
+from .tasks import validate_credit
+from django.urls import reverse
+import json
+
+
+logger = logging.getLogger(__name__)
 
 
 class CreditTestCase(APISimpleTestCase):
     def setUp(self):
+        logging.disable(logging.DEBUG)
         self.credit_check_url = '/api/v1/credit/'
-
+        self.results_url = '/api/v1/results/'
+        self.valid_credit_data = {
+                'user_age': 19,
+                'credit_value': 50000
+                }
+        
     def test_credit_check_valid_payload(self):
-        valid_credit_data = {
-            'user_age': 19,
-            'credit_value': 50000
-        }
-        response = self.client.post(self.credit_check_url, data=valid_credit_data)
+        logger.info("Tests are running, it may takes until 40 seconds to complete...")
+        response = self.client.post(self.credit_check_url, data=self.valid_credit_data)
+        content = json.loads(response.content)
+        ticket_id = content["ticket_id"]
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response)
-        print(response.data[0])
+        assert ticket_id
+
+        response = self.client.get(reverse("results", args=[ticket_id]))
+        content = json.loads(response.content)
+        if content["ticket_status"] == "STARTED":
+            assert content == {'ticket_id': ticket_id, 'ticket_status': 'STARTED', 'result': 'The credit is being checked right now, please try again in a few seconds.'}
+        if content["ticket_status"] == "PENDING":
+            assert content == {'ticket_id': ticket_id, 'ticket_status': 'PENDING', 'result': 'The credit check task is enqueued or ticket does not exist.'}
+        while content["ticket_status"] != "SUCCESS":
+            response = self.client.get(reverse("results", args=[ticket_id]))
+            content = json.loads(response.content)
+        assert content == {'ticket_id': ticket_id, 'ticket_status': 'SUCCESS', 'result': 'Credit approved: All values match the requirements.'}
+        assert response.status_code == 200
+
+    def test_serializer(self):
+        serializer = CreditCheckSerializer()
+        data = serializer.validate(self.valid_credit_data)
+
+        self.assertEqual(data, self.valid_credit_data)
 
