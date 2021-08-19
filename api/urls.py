2e4621732f7335fa65ae2c29d8b7f0d626d99eee
João Paulo Cardoso <jpaulo.jpc@gmail.com>
2021-08-19 00:25:00 -0300

    modifying urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import CreditCheck, Results
 
 
 urlpatterns = [
-    path('credit/', CreditCheck.as_view(), name='credit check'),
+    path('credit/', CreditCheck.as_view(), name='credit_check'),
     path('results/<str:ticket>/', Results.as_view(), name='results'),
 ]
