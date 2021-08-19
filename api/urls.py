366dc93a193a8cba2d981b72ac6bba0107f434fe
João Paulo Cardoso <jpaulo.jpc@gmail.com>
2021-08-19 10:57:45 -0300

    modifying endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('credit-check/', CreditCheck.as_view(), name='credit_check'),
-    path('credit-check/tickets/<str:ticket>/', Results.as_view(), name='results'),
+    path('api/v1/credit-check/', CreditCheck.as_view(), name='credit_check'),
+    path('api/v1/credit-check/tickets/<str:ticket>/', Results.as_view(), name='results'),
     path('', views.api_root)
 ]
